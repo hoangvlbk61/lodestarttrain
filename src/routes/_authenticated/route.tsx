c5f7d198b677b0ab9fefb8b0cb915722b3de41f8
hoangvl <hoang1.vule@toshiba.co.jp>
2026-01-28 19:09:36 +0700

    Login: wip

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { AuthenticatedLayout } from '@/components/layout/authenticated-layout'
 
 export const Route = createFileRoute('/_authenticated')({
+  beforeLoad: ({ context, location }) => {
+    // Truy cập trực tiếp auth state từ context của router
+    // @ts-ignore (Giả định context có chứa auth)
+    if (!context.auth.isAuthenticated) {
+      throw redirect({
+        to: '/sign-in',
+        search: { redirect: location.href },
+      })
+    }
+  },
   component: AuthenticatedLayout,
 })
